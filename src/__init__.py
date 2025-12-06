2f042f107e6f06e481be2936ca3f5de6081cb516
YarVolkotrub <at2tzik@gmail.com>
2025-12-06 04:22:07 +0300

    up

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,6 @@
-__all__: list[str] = ["PLUGIN_TEMPLATE", "PLUGIN_DIR_NAME"]
+from .IPlugin import IPlugin
+
+__all__: list[str] = ["IPlugin", "PLUGIN_TEMPLATE", "PLUGIN_DIR_NAME"]
 
 PLUGIN_TEMPLATE: str = '/*/plugin*.py'
 PLUGIN_DIR_NAME: str = 'plugins'
